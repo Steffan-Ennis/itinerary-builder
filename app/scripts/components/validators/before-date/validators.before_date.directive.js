220b978c8a1905fd5a14330a2713b3557125f217
Steffan Ennis <steffanennis87@gmail.com>
2016-08-17 08:32:24 +0930

    before date validation

--- app/scripts/components/validators/before-date/validators.before_date.directive.js
+++ app/scripts/components/validators/before-date/validators.before_date.directive.js
@@ -1,4 +1,7 @@
 /**
+ * Created by steffanennis on 17/08/2016.
+ */
+/**
  * Created by steffanennis on 16/08/2016.
  */
 (function(){
@@ -11,29 +14,29 @@
       beforeDate
     ]);
 
-    function beforeDate(moment) {
-      var directive = {
-        restrict: 'A',
-        require: 'ngModel',
-        link: link,
-        scope: {
-          beforeDate: '=',
-        }
-      };
+  function beforeDate(moment) {
+    var directive = {
+      restrict: 'A',
+      require: 'ngModel',
+      link: link,
+      scope: {
+        beforeDate: '=',
+      }
+    };
 
-      return directive;
+    return directive;
 
-      function link(scope,elem,attr,ngModel) {
-        ngModel.$validators.beforeDate = function (modelValue, viewValue) {
-          if (moment.isDate(scope.beforeDate) && moment(modelValue).isAfter(scope.beforeDate)){
-            return true;
-          } else {
-            return false;
-          }
+    function link(scope,elem,attr,ngModel) {
+      ngModel.$validators.beforeDate = function (modelValue, viewValue) {
+        if (moment.isDate(scope.beforeDate) && moment(modelValue).isBefore(scope.beforeDate)){
+          return true;
+        } else {
+          return false;
         }
       }
+    }
 
-    };
+  };
 
 
 }());

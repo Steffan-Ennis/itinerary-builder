34ea35a7ee28ff8ede06576072d3e6056d52d3c2
Steffan Ennis <steffanennis87@gmail.com>
2016-08-17 08:32:09 +0930

    Refactored: makes more sense to call the directive afterDate

---
+++ app/scripts/components/validators/after-date/validators.after_date.directive.js
@@ -0,0 +1,39 @@
+/**
+ * Created by steffanennis on 16/08/2016.
+ */
+(function(){
+  'use strict';
+
+  angular.module('validators')
+
+    .directive('afterDate',[
+      'moment',
+      afterDate
+    ]);
+
+    function afterDate(moment) {
+      var directive = {
+        restrict: 'A',
+        require: 'ngModel',
+        link: link,
+        scope: {
+          afterDate: '=',
+        }
+      };
+
+      return directive;
+
+      function link(scope,elem,attr,ngModel) {
+        ngModel.$validators.afterDate = function (modelValue, viewValue) {
+          if (moment.isDate(scope.afterDate) && moment(modelValue).isAfter(scope.afterDate)){
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }
+
+    };
+
+
+}());
